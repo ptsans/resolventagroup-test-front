9f33b7fa0282dde466087e029e8b2b987984fa0a
ptsans <kzone416@gmail.com>
2021-03-22 20:43:46 +0800

    fix: update useCollection hook

--- src/hooks/useCollection.ts
+++ src/hooks/useCollection.ts
@@ -1,5 +1,5 @@
 import useSWR from "swr";
-import {Character} from "../types/Character";
+import {Character, Gender, Status} from "../types/Character";
 
 type CollectionResponse = {
     info: {
@@ -8,10 +8,39 @@ type CollectionResponse = {
     results: Character[]
 }
 
+export type CollectionFilter = {
+    name: string,
+    gender: Gender,
+    status: Status
+}
+
 const fetcher = (url: string): Promise<CollectionResponse> => fetch(url).then(r => r.json())
 
-export function useCollection(collectionName: string, page: number) {
-    const { data, error } = useSWR(`https://rickandmortyapi.com/api/${collectionName}/?page=${page}`, fetcher);
+const getKey = (
+    collectionName: string,
+    page: number,
+    filter: CollectionFilter|undefined
+): string => {
+    let baseUrl = `https://rickandmortyapi.com/api/${collectionName}/?page=${page}`
+
+    if (filter) {
+        const {name, status, gender} = filter
+        if (name !== '') {
+            baseUrl += `&name=${encodeURIComponent(name.trim().toLocaleLowerCase())}`
+        }
+        if (status !== Status.All) {
+            baseUrl += `&status=${encodeURIComponent(status.toLocaleLowerCase())}`
+        }
+        if (gender !== Gender.All) {
+            baseUrl += `&gender=${encodeURIComponent(gender.toLocaleLowerCase())}`
+        }
+    }
+
+    return baseUrl
+}
+
+export function useCollection(collectionName: string, page: number, filter: CollectionFilter|undefined) {
+    const { data, error } = useSWR(getKey(collectionName, page, filter), fetcher);
     let collection, pages
 
     if (data && !error) {
