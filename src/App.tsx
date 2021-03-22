9b235ed306ced0b69470ee4c1dd88e9fc7110bbc
ptsans <kzone416@gmail.com>
2021-03-22 20:45:27 +0800

    feat: fill home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,59 @@
 import * as React from 'react';
 
+import {CollectionFilter, useCollection} from "./hooks/useCollection";
+import {useCallback, useEffect, useState} from "react";
+import {Container, Pagination} from "@material-ui/core";
+import Card from "./components/Card";
+import Grid from "./components/Grid";
+import Appbar from "./components/Appbar";
+import Favorites from "./components/Favorites";
+import {useLocalCollection} from "./hooks/useLocalCollection";
+
 export default function App() {
-  return <h1>Hello guys!</h1>
+    const [page, setPage] = useState(1)
+    const [filter, setFilter] = useState<CollectionFilter>()
+    const { collection, pages } = useCollection('character', page, filter)
+    const { saved } = useLocalCollection('characters')
+
+    const filterUpdate = useCallback((filter: CollectionFilter) => {
+        setFilter(filter)
+        setPage(1)
+    }, [])
+
+    return (
+        <Container
+            maxWidth="md"
+            sx={{
+              padding: '2rem 0'
+            }}
+        >
+            <Appbar
+                filterUpdate={filterUpdate}
+            />
+            <Favorites />
+            {
+                collection && (
+                    <>
+                        <Grid>
+                            {
+                                collection.map((character) => {
+                                    return <Card
+                                        character={character}
+                                        key={character.id}
+                                        isExist={!!saved[character.id]}
+                                    />
+                                })
+                            }
+                        </Grid>
+                        <Pagination
+                            page={page}
+                            count={pages}
+                            color="primary"
+                            onChange={(event, page) => setPage(page)}
+                        />
+                    </>
+                )
+            }
+      </Container>
+    )
 }
